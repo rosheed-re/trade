4ca5dc656bd2ad2a5448fe9eccb27160e1440a19
rosheed-re <harrytat09@gmail.com>
2026-06-25 05:12:03 -0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
 import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
-import Portfolio from './pages/Portofolio.jsx'
+import Portfolio from './pages/Portfolio.jsx'
 import Services from './pages/Services.jsx'
 import Contact from './pages/Contact.jsx'
 
